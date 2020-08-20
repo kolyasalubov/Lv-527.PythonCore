58e20fe579d60cefa9221479d4a19f1297e48d56
Vitalii Yremchuk <montybanks.mb@gmail.com>
2020-08-20 17:38:21 +0300

    added HW

---
+++ HW1.py
@@ -0,0 +1,12 @@
+print ("Hello, please, input only int numbers")
+a = int(input("Now define a: "))
+b = int(input("Now define b: "))
+
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a * b = ", a * b)
+print("a / b = ", a / b)
+print("a**b = ", a**b)
+
+print("Thanks for using!")
